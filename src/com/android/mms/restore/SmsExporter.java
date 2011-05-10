ea5c2e505f301c58bc691922b0238a0df420e84a
geesun <geesun@gmail.com>
2011-05-10 07:13:27 -0700

    add chinese

--- src/com/android/mms/restore/SmsExporter.java
+++ src/com/android/mms/restore/SmsExporter.java
@@ -32,7 +32,7 @@ public class SmsExporter {
 	private final int mFileIndexMaximum = 100;
 	
 	private final String mFileNameExtension = "sms";
-	private final String mFileNamePrefix = "androidin_";
+	private final String mFileNamePrefix = "opda_";
 	private final String mFileNameSuffix = "";
 	private final String mTargetDirectory = "/sdcard";
 	
